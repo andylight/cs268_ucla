a90320de5a44391f6491f1175312a68d8add9bb8
Eric Kim <erickim555@gmail.com>
2013-11-28 02:59:37 -0800

    Removed some unnecessary lines in detect_lanes.py

--- hw4/detect_lanes.py
+++ hw4/detect_lanes.py
@@ -78,66 +78,17 @@ def detect_lanes(I, win1=(0.4, 0.55, 0.2, 0.1), win2=(0.6, 0.55, 0.2, 0.1),
     # Fix line to be in image coordinate system (not window coord sys)
     if line1_norm != None:
         a1, b1, c1 = line1_norm
-        #c1_out = -a1*(x_left-(w_left/2)) - b1*((h-1-y_left)-(h_left/2)) + c1
         c1_out = -a1*(x_left-(w_left/2)) - b1*((y_left)-(h_left/2)) + c1
         line1_out = np.array([a1, b1, c1_out])
     else:
         line1_out = None
     if line2_norm != None:
         a2, b2, c2 = line2_norm
-        #c2_out = -a2*(x_right-(w_right/2)) - b2*((h-1-y_right)-(h_right/2)) + c2
         c2_out = -a2*(x_right-(w_right/2)) - b2*((y_right)-(h_right/2)) + c2
         line2_out = np.array([a2, b2, c2_out])
     else:
         line2_out = None
     return line1_out, line2_out
-
-def plot_lines(I, line1, line2):
-    """ Plots lines on the input image.
-    Input:
-        nparray I
-        tuple line1, line2: (float a, float b, float c)
-    Output:
-        nparray Iout
-    """
-    # TODO: Working nuking this, this is BAD! See overlay_line().
-    Irgb = util.to_rgb(I)
-    if line1 != None:
-        Irgb = overlay_line(Irgb, line1, colour=(255, 0, 0))
-    if line2 != None:
-        Irgb = overlay_line(Irgb, line2, colour=(0, 255, 0))
-    return Irgb
-    
-def overlay_line(I, line, colour=(255, 0, 0), thick=1):
-    """ Adds a colored line to the input image.
-    Input:
-        nparray I: 
-            If I is not RGB, this will convert it.
-        tuple line: (float a, float b, float c)
-        tuple clr: (int R, int G, int B)
-    Output:
-        nparray I_rgb
-    """
-    # TODO: This uses lines in 'normal' coord system. BAD! I'm
-    # working on nuking this in favor of util_camera.draw_line()
-    a, b, c = line # ax + by + c = 0 => y = (-ax - c) / b
-    Irgb = util.to_rgb(I)
-    h, w = Irgb.shape[0:2]
-    for x in xrange(w):
-        y = (-a*x - c) / b
-        y = (h-1) - y # Remember: lines are in 'normal' coord system,
-                      # not image coord system (where origin is UL-corner)
-        if y < 0:
-            continue
-        y_low = max(0, y - thick)
-        y_high = min(h-1, y + thick)
-        x_low = max(0, x - thick)
-        x_high = min(w-1, x + thick)
-        Irgb[y_low:y_high, x_low:x_high, 0] = colour[0]
-        Irgb[y_low:y_high, x_low:x_high, 1] = colour[1]
-        Irgb[y_low:y_high, x_low:x_high, 2] = colour[2]
-
-    return Irgb
 
 def draw_subwindow(Irgb, win, colour=(125, 125, 0)):
     """ Draws subwindow on Irgb.
